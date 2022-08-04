ff1febcbcdbf5b5b44397e0116ae02f991a370f3
Web Developer <useraccount@WebDeveloperMBP.local>
2022-08-04 10:51:18 -0500

    Add Overlay and main menu reset function - LM

--- react/HeaderMenuNav.tsx
+++ react/HeaderMenuNav.tsx
@@ -15,6 +15,8 @@ interface HeaderMenuNavProps {
 const HeaderMenuNav: StorefrontFunctionComponent<HeaderMenuNavProps> = ({ mainMenu, children }) => {
   const parentNav = useRef(null);
   const submenuRef = useRef<any>(null);
+  const overlayRef = useRef<any>(null);
+
   const [menu, setMenu] = useState<any>();
   const [menuNumber, setMenuNumber] = useState<any>();
   const [headerHeight, setHeaderHeight] = useState<number>(0);
@@ -22,15 +24,23 @@ const HeaderMenuNav: StorefrontFunctionComponent<HeaderMenuNavProps> = ({ mainMe
   const classPrefix = "eriksbikeshop-headermenunav-1-x-";
   const subMenuBorderClass = "subMenuBorder";
   const mainMenuBorderClass = "mainMenuBorder";
+  const darkOverlayClass = "darkOverlay";
+  const displayNoneClass = "displayNone";
 
   useOnClickOutside(parentNav, () => closeSubmenu());
 
   useEffect(() => {
-    console.clear();
-    if (menu === undefined && submenuRef.current) {
+    // console.clear();
+
+    // Toggle classes for menu display --
+    if (menu === undefined && verifyRefs()) {
       submenuRef.current.classList.remove(classPrefix + subMenuBorderClass);
+      overlayRef.current.classList.remove(classPrefix + darkOverlayClass);
+      overlayRef.current.classList.add(classPrefix + displayNoneClass);
     } else {
       submenuRef.current.classList.add(classPrefix + subMenuBorderClass);
+      overlayRef.current.classList.add(classPrefix + darkOverlayClass);
+      overlayRef.current.classList.remove(classPrefix + displayNoneClass);
     }
 
     removeMainMenuUnderline();
@@ -38,8 +48,18 @@ const HeaderMenuNav: StorefrontFunctionComponent<HeaderMenuNavProps> = ({ mainMe
     // @ts-expect-error
     const openMenu = document.querySelector(`[data-menunumber="${menuNumber}"]`);
     openMenu?.classList.add(classPrefix + mainMenuBorderClass);
-
   })
+
+  const verifyRefs = () => {
+    const refsToVerify = [submenuRef, overlayRef];
+    let valueToReturn = true;
+
+    refsToVerify.forEach(ref => {
+      if (!ref.current) valueToReturn = false;
+    })
+
+    return valueToReturn;
+  }
 
   const handleClick = (e: any) => {
     const menuClicked = Number(e.target.dataset.menunumber);
@@ -52,6 +72,15 @@ const HeaderMenuNav: StorefrontFunctionComponent<HeaderMenuNavProps> = ({ mainMe
     setMenu(children[menuClicked]);
 
     setHeaderHeight(fullHeaderHeight);
+
+    // Reset Menu if already active. Useful for returning to the main menu from a submenu --
+    if (menuNumber === menuClicked) {
+      setMenu(undefined);
+      setTimeout(() => {
+        // @ts-expect-error
+        setMenu(children[menuClicked]);
+      }, 1)
+    }
   }
 
   const closeSubmenu = () => {
@@ -68,7 +97,6 @@ const HeaderMenuNav: StorefrontFunctionComponent<HeaderMenuNavProps> = ({ mainMe
     }
   }
 
-
   return (
     <div ref={parentNav} className={styles.mainMenuContainer}>
       <nav className={styles.mainMenuWrapper}>
@@ -82,6 +110,7 @@ const HeaderMenuNav: StorefrontFunctionComponent<HeaderMenuNavProps> = ({ mainMe
         <div ref={submenuRef} className={styles.subMenuWrapper} style={{ top: `${headerHeight}px` }}>
           {menu}
         </div>
+        <div ref={overlayRef} onClick={closeSubmenu} style={{ top: `${headerHeight}px` }}></div>
       </div>
     </div>
   )
